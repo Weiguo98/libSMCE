c20f0e1222ee987381cb23fc699ba4662606642c
Elena <elena@manele.se>
2021-10-02 19:30:41 +0200

    add _b to byte litterals + test

--- test/BoardView.cpp
+++ test/BoardView.cpp
@@ -29,6 +29,9 @@
 
 using namespace std::literals;
 
+constexpr std::byte operator""_b(char c) noexcept { return static_cast<std::byte>(c); }
+constexpr std::byte operator""_b(unsigned long long c) noexcept { return static_cast<std::byte>(c); }
+
 TEST_CASE("BoardView GPIO", "[BoardView]") {
     smce::Toolchain tc{SMCE_PATH};
     REQUIRE(!tc.check_suitable_environment());
@@ -134,13 +137,13 @@ TEST_CASE("BoardView UART", "[BoardView]") {
 TEST_CASE("BoardView FrameBuffer RGB444 Write", "[BoardView]") {
     // set up
     std::byte target[3]; // target
-    std::byte black_bits[] = {(std::byte)0, (std::byte)0};
-    std::byte white_bits[] = {(std::byte)0xff, (std::byte)0xf};
-    std::byte red_bits[] = {(std::byte)0, (std::byte)0xf};
-    std::byte green_bits[] = {(std::byte)0xf0, (std::byte)0};
-    std::byte blue_bits[] = {(std::byte)0x0f, (std::byte)0};
-    std::byte brown_bits[] = {(std::byte)0x75, (std::byte)0x8};
-    std::byte purple_bits[] = {(std::byte)0x5c, (std::byte)0xa};
+    std::byte black_bits[] = {0_b, 0_b};
+    std::byte white_bits[] = {0xff_b, 0xf_b};
+    std::byte red_bits[] = {0_b, 0xf_b};
+    std::byte green_bits[] = {0xf0_b, 0_b};
+    std::byte blue_bits[] = {0x0f_b, 0_b};
+    std::byte brown_bits[] = {0x75_b, 0x8_b};
+    std::byte purple_bits[] = {0x5c_b, 0xa_b};
 
     smce::convert_rgb444_to_rgb888(std::span{black_bits, 2}, target);
     REQUIRE((int)target[0] == 0); // red
@@ -182,13 +185,13 @@ TEST_CASE("BoardView FrameBuffer RGB444 Write", "[BoardView]") {
 TEST_CASE("BoardView FrameBuffer RGB444 Read", "[BoardView]") {
     // set up
     std::byte target[2]; // target
-    std::byte black_bits[] = {(std::byte)0, (std::byte)0, (std::byte)0};
-    std::byte white_bits[] = {(std::byte)0xff, (std::byte)0xff, (std::byte)0xff};
-    std::byte red_bits[] = {(std::byte)0xff, (std::byte)0, (std::byte)0};
-    std::byte green_bits[] = {(std::byte)0, (std::byte)0xff, (std::byte)0};
-    std::byte blue_bits[] = {(std::byte)0x0, (std::byte)0, (std::byte)0xff};
-    std::byte brown_bits[] = {(std::byte)0x95, (std::byte)0x6c, (std::byte)0x32};
-    std::byte purple_bits[] = {(std::byte)0xa4, (std::byte)0x52, (std::byte)0xcd};
+    std::byte black_bits[] = {0_b, 0_b, 0_b};
+    std::byte white_bits[] = {0xff_b, 0xff_b, 0xff_b};
+    std::byte red_bits[] = {0xff_b, 0_b, 0_b};
+    std::byte green_bits[] = {0_b, 0xff_b, 0_b};
+    std::byte blue_bits[] = {0x0_b, 0_b, 0xff_b};
+    std::byte brown_bits[] = {0x95_b, 0x6c_b, 0x32_b};
+    std::byte purple_bits[] = {0xa4_b, 0x52_b, 0xcd_b};
 
     smce::convert_rgb888_to_rgb444(black_bits, target);
     REQUIRE((int)target[1] == 0); // red
@@ -223,13 +226,13 @@ TEST_CASE("BoardView FrameBuffer RGB444 Read", "[BoardView]") {
 TEST_CASE("BoardView FrameBuffer RGB565 Read", "[BoardView]") {
     // set up
     std::byte target[2]; // target
-    std::byte black_bits[] = {(std::byte)0, (std::byte)0, (std::byte)0};
-    std::byte white_bits[] = {(std::byte)0xff, (std::byte)0xff, (std::byte)0xff};
-    std::byte red_bits[] = {(std::byte)0xff, (std::byte)0, (std::byte)0};
-    std::byte green_bits[] = {(std::byte)0, (std::byte)0xff, (std::byte)0};
-    std::byte blue_bits[] = {(std::byte)0x0, (std::byte)0, (std::byte)0xff};
-    std::byte brown_bits[] = {(std::byte)0x95, (std::byte)0x6c, (std::byte)0x32};
-    std::byte purple_bits[] = {(std::byte)0xa4, (std::byte)0x52, (std::byte)0xcd};
+    std::byte black_bits[] = {0_b, 0_b, 0_b};
+    std::byte white_bits[] = {0xff_b, 0xff_b, 0xff_b};
+    std::byte red_bits[] = {0xff_b, 0_b, 0_b};
+    std::byte green_bits[] = {0_b, 0xff_b, 0_b};
+    std::byte blue_bits[] = {0x0_b, 0_b, 0xff_b};
+    std::byte brown_bits[] = {0x95_b, 0x6c_b, 0x32_b};
+    std::byte purple_bits[] = {0xa4_b, 0x52_b, 0xcd_b};
 
     smce::convert_rgb888_to_rgb565(black_bits, target);
     REQUIRE((int)target[1] == 0); // red
@@ -264,13 +267,13 @@ TEST_CASE("BoardView FrameBuffer RGB565 Read", "[BoardView]") {
 TEST_CASE("BoardView FrameBuffer RGB565 Write", "[BoardView]") {
     // set up
     std::byte target[3];
-    std::byte black_bits[] = {(std::byte)0, (std::byte)0};
-    std::byte white_bits[] = {(std::byte)0xff, (std::byte)0xff};
-    std::byte red_bits[] = {(std::byte)0x00, (std::byte)0xf8};
-    std::byte green_bits[] = {(std::byte)0xe0, (std::byte)0x07};
-    std::byte blue_bits[] = {(std::byte)0x1f, (std::byte)0};
-    std::byte brown_bits[] = {(std::byte)0x66, (std::byte)0x93};
-    std::byte purple_bits[] = {(std::byte)0x99, (std::byte)0xa2};
+    std::byte black_bits[] = {0_b, 0_b};
+    std::byte white_bits[] = {0xff_b, 0xff_b};
+    std::byte red_bits[] = {0x00_b, 0xf8_b};
+    std::byte green_bits[] = {0xe0_b, 0x07_b};
+    std::byte blue_bits[] = {0x1f_b, 0_b};
+    std::byte brown_bits[] = {0x66_b, 0x93_b};
+    std::byte purple_bits[] = {0x99_b, 0xa2_b};
 
     smce::convert_rgb565_to_rgb888(std::span{black_bits, 2}, target);
     REQUIRE((int)target[0] == 0); // red
@@ -306,4 +309,96 @@ TEST_CASE("BoardView FrameBuffer RGB565 Write", "[BoardView]") {
     REQUIRE((int)target[0] == 0xa5); // red
     REQUIRE((int)target[1] == 0x51); // green
     REQUIRE((int)target[2] == 0xce); // blue
+}
+
+TEST_CASE("BoardView RGB444 cvt", "[BoardView]") {
+    smce::Toolchain tc{SMCE_PATH};
+    REQUIRE(!tc.check_suitable_environment());
+    smce::Sketch sk{SKETCHES_PATH "noop", {.fqbn = "arduino:avr:nano"}};
+    const auto ec = tc.compile(sk);
+    if (ec)
+        std::cerr << tc.build_log().second;
+    REQUIRE_FALSE(ec);
+    smce::Board br{};
+    REQUIRE(br.configure({.frame_buffers = {{}}}));
+    REQUIRE(br.attach_sketch(sk));
+    REQUIRE(br.start());
+    auto bv = br.view();
+    REQUIRE(bv.valid());
+    REQUIRE(br.suspend());
+    auto fb = bv.frame_buffers[0];
+    REQUIRE(fb.exists());
+
+    {
+        constexpr std::size_t height = 1;
+        constexpr std::size_t width = 1;
+
+        constexpr std::array in = {'\xBC'_b, '\x0A'_b};
+        constexpr std::array expected_out = {'\xAA'_b, '\xBB'_b, '\xCC'_b};
+        static_assert(in.size() == expected_out.size() / 3 * 2);
+
+        fb.set_height(height);
+        fb.set_width(width);
+        REQUIRE(fb.write_rgb444(in));
+
+        std::array<std::byte, std::size(expected_out)> out;
+        REQUIRE(fb.read_rgb888(out));
+        REQUIRE(out == expected_out);
+    }
+
+    {
+        constexpr std::size_t height = 2;
+        constexpr std::size_t width = 2;
+
+        constexpr std::array in = {'\x23'_b, '\xF1'_b, '\x56'_b, '\xF4'_b, '\x89'_b, '\xF7'_b, '\xBC'_b, '\xFA'_b};
+        constexpr std::array expected_out = {'\x11'_b, '\x22'_b, '\x33'_b, '\x44'_b, '\x55'_b, '\x66'_b,
+                                             '\x77'_b, '\x88'_b, '\x99'_b, '\xAA'_b, '\xBB'_b, '\xCC'_b};
+        static_assert(in.size() == expected_out.size() / 3 * 2);
+
+        fb.set_height(height);
+        fb.set_width(width);
+        fb.write_rgb444(in);
+
+        std::array<std::byte, std::size(expected_out)> out;
+        fb.read_rgb888(out);
+        REQUIRE(out == expected_out);
+    }
+
+    {
+        constexpr std::size_t height = 1;
+        constexpr std::size_t width = 1;
+
+        constexpr std::array in = {'\xAD'_b, '\xBE'_b, '\xCF'_b};
+        constexpr std::array expected_out = {'\xBC'_b, '\x0A'_b};
+        static_assert(expected_out.size() == in.size() / 3 * 2);
+
+        fb.set_height(height);
+        fb.set_width(width);
+        REQUIRE(fb.write_rgb888(in));
+
+        std::array<std::byte, std::size(expected_out)> out;
+        REQUIRE(fb.read_rgb444(out));
+        REQUIRE(out == expected_out);
+    }
+
+    {
+        constexpr std::size_t height = 2;
+        constexpr std::size_t width = 2;
+
+        constexpr std::array in = {'\x1A'_b, '\x2B'_b, '\x3C'_b, '\x4D'_b, '\x5E'_b, '\x6F'_b,
+                                   '\x7A'_b, '\x8B'_b, '\x9C'_b, '\xAD'_b, '\xBE'_b, '\xCF'_b};
+        constexpr std::array expected_out = {'\x23'_b, '\x01'_b, '\x56'_b, '\x04'_b,
+                                             '\x89'_b, '\x07'_b, '\xBC'_b, '\x0A'_b};
+        static_assert(expected_out.size() == in.size() / 3 * 2);
+
+        fb.set_height(height);
+        fb.set_width(width);
+        fb.write_rgb888(in);
+
+        std::array<std::byte, std::size(expected_out)> out;
+        fb.read_rgb444(out);
+        REQUIRE(out == expected_out);
+    }
+
+    REQUIRE(br.stop());
 }
